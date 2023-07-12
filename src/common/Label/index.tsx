6b514ede2dbfc1b37ad6244778a9034fe3b42435
Agustin Marengo <agustinmrgo@gmail.com>
2023-07-12 19:17:58 +0200

    plugin install to resolve imports

--- src/common/Label/index.tsx
+++ src/common/Label/index.tsx
@@ -3,12 +3,10 @@ import { Text, TextProps } from 'react-native';
 
 import styles from './styles';
 
-const Label: React.FunctionComponent<TextProps> = ({ children, style, ...props }) => {
-  return (
-    <Text style={[styles.label, style]} {...props}>
-      {children}
-    </Text>
-  );
-};
+const Label: React.FunctionComponent<TextProps> = ({ children, style, ...props }) => (
+  <Text style={[styles.label, style]} {...props}>
+    {children}
+  </Text>
+);
 
 export default Label;
